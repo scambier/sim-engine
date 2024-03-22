3a6a2beea3ca6729e2be4c2c5ac81b92bdee1b46
Simon Cambier <simon.cambier@protonmail.com>
2024-03-22 17:03:37 +0100

    print() with colors, cls()

--- game/main.js
+++ game/main.js
@@ -1,3 +1,6 @@
 function update() {
-    print('Hello from Boa!', 10, 10);
+    cls();
+    print('default values');
+    print('Hello from Boa!', 16, 16, 0xff0000);
+    print('Alpha transparency', 16, 32, 0x40ff0000);
 }
